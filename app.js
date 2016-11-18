498cb7f438e45e6c53334258f060881cd5f99fef
Maithrika Sara <maithrika@Consciously-Uncoupling.local>
2016-11-17 21:27:49 -0800

    added basic objects and data

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var projects = [];
+
+function Project(proj) {
+  this.title = proj.title;
+  this.category = proj.category;
+  this.year = proj.year;
+  this.language = proj.language;
+  this.description = proj.description;
+  this.url = proj.url;
+  this.preview = proj.preview;
+}
---
+++ data.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var myProjects = [
+  {
+    title: 'Guessing Game',
+    category: 'individual',
+    year: 2016,
+    language: 'HTML/CSS',
+    description: '',
+    url: 'https://m-sara.github.io/guessing_game/',
+    preview: ''
+  },
+  {
+    title: 'Campfire Coffee',
+    category: 'individual',
+    year: 2016,
+    language: 'HTML/CSS/JS',
+    description: '',
+    url: 'https://m-sara.github.io/campfire_coffee/',
+    preview: ''
+  },
+  {
+    title: 'Chocolate Pizza',
+    category: 'partner',
+    year: 2016,
+    language: 'HTML/CSS',
+    description: '',
+    url: 'https://m-sara.github.io/chocolate-pizza/',
+    preview: ''
+  },
+  {
+    title: 'BusMall',
+    category: 'individual',
+    year: 2016,
+    language: 'HTML/CSS/JS',
+    description: '',
+    url: 'https://m-sara.github.io/bus-mall/',
+    preview: ''
+  },
+  {
+    title: 'Seattle Outreach Services',
+    category: 'group',
+    year: 2016,
+    language: 'HTML/CSS/JS',
+    description: '',
+    url: 'https://dkulp23.github.io/sos/',
+    preview: ''
+  }
+];
