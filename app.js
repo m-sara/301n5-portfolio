b155f6d9e99b4df4e57e48671733623604c102fa
Maithrika Sara <maithrika@Consciously-Uncoupling.local>
2016-11-19 17:42:58 -0800

    added ghost for sections

--- app.js
+++ app.js
@@ -21,4 +21,35 @@ projectView.handleMainNav = function() {
   $('.nav-menu .tab:first').click();
 };
 
+// projectView.populateFilters = function() {
+//   $('project').not('.template').each(function() {
+//     var authorName, category, optionTag;
+//     authorName = $(this).find('address a').text();
+//     optionTag = '<option value="' + authorName + '">' + authorName + '</option>';
+//     $('#author-filter').append(optionTag);
+//     category = $(this).attr('data-category');
+//     optionTag = '<option value="' + category + '">' + category + '</option>';
+//     if ($('#category-filter option[value="' + category + '"]').length === 0) {
+//       $('#category-filter').append(optionTag);
+//     }
+//   });
+// };
+
+// projectView.handleCategoryFilter = function() {
+//   $('#category-filter').on('change', function() {
+//     if ($(this).val()) {
+//       $('project').hide();
+//       $(`project[data-category="${ $(this).val() }"]`).fadeIn(500);
+//     } else {
+//       $('project').not('.template').show();
+//     }
+//
+//     $('#author-filter').val('');
+//   });
+// };
+
+
+
+////////////////////////////////////////////////////////
+
 projectView.handleMainNav();
